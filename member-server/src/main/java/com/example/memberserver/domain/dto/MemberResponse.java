d863e9a8bb4dd7d662dcb9afb542b91f583311e7
김진수 <jinsu.kim@mirero.co.kr>
2024-11-18 17:54:40 +0900

    refactor: 기존 레이어드 아키텍처 코드 삭제

--- member-server/src/main/java/com/example/memberserver/domain/dto/MemberResponse.java
+++
@@ -1,4 +0,0 @@
-package com.example.memberserver.domain.dto;
-
-public record MemberResponse(Long memberId, String name) {
-}
--- member-server/src/main/java/com/example/memberserver/domain/dto/MemberSaveRequest.java
+++
@@ -1,6 +0,0 @@
-package com.example.memberserver.domain.dto;
-
-import com.example.common.role.Role;
-
-public record MemberSaveRequest(String loginId, String password, String name, Role role) {
-}
